7f1cfa189ee7eeecbf2de9d0a1522cc79187f14e
dinhquocnhat <nhatd87@gmail.com>
2022-12-19 15:31:56 +0700

    nhat chinh sua import file excel va chinh lai folder trong vnsapo product

--- sapo-product/src/main/java/vn/sapo/brand/dto/BrandResult.java
+++ sapo-product/src/main/java/vn/sapo/brand/dto/BrandResult.java
@@ -1,15 +1,17 @@
-package vn.sapo.category.dto;
+package vn.sapo.brand.dto;
 
 import lombok.*;
 import lombok.experimental.Accessors;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Accessors(chain = true)
-public class CategoryResult {
+public class BrandResult {
     private Integer id;
     private String title;
 
+    @Override
+    public String toString() {
+        return  title ;
+    }
 }
---
+++ sapo-product/src/main/java/vn/sapo/excel/ExcelService.java
@@ -0,0 +1,28 @@
+package vn.sapo.excel;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import vn.sapo.entities.product.Product;
+import vn.sapo.product.ProductMapper;
+import vn.sapo.product.ProductRepository;
+
+@Service
+public class ExcelService {
+    @Autowired
+    ProductRepository repository;
+    @Autowired
+    ProductMapper productMapper;
+
+    public void save(MultipartFile file) {
+        try {
+            List<Product> products = ExcelHelper.excelToProducts(file.getInputStream());
+            repository.saveAll(products);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store excel data: " + e.getMessage());
+        }
+    }
+}
