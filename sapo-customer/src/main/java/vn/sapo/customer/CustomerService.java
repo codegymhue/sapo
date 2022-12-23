0b0427425e67d1c3cc7847aa4eb5eaca7987a4fd
6Thienthien6 <thien502760@gmail.com>
2022-12-23 16:55:16 +0700

    comit merge code

--- sapo-customer/src/main/java/vn/sapo/customer/CustomerService.java
+++ sapo-customer/src/main/java/vn/sapo/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package vn.sapo.customer;
 
+import org.springframework.transaction.annotation.Transactional;
 import vn.sapo.customer.dto.CreateCustomerParam;
 import vn.sapo.customer.dto.CustomerResult;
 import vn.sapo.customer.dto.UpdateCustomerParam;
@@ -11,6 +12,9 @@ public interface CustomerService {
     CustomerResult findById(Integer id);
 
     List<CustomerResult> findAll();
+
+    @Transactional(readOnly = true)
+//    List<CustomerResult> findCustomerByStatus();
 
     CustomerResult create(CreateCustomerParam customerCreate);
 
