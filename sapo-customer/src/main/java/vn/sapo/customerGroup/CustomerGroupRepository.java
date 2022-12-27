44b94f6dbb9ed80e041178ee99098c320161a18f
Quang Huy <2hnono30@gmail.com>
2022-12-27 09:30:16 +0700

    fix customerResult

--- sapo-customer/src/main/java/vn/sapo/customerGroup/CustomerGroupRepository.java
+++ sapo-customer/src/main/java/vn/sapo/customerGroup/CustomerGroupRepository.java
@@ -1,10 +1,21 @@
 package vn.sapo.customerGroup;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import vn.sapo.customerGroup.dto.CustomerGroupResult;
 import vn.sapo.entities.customer.CustomerGroup;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerGroupRepository  extends JpaRepository<CustomerGroup, Integer> {
-
+    @Query("select new vn.sapo.customerGroup.dto.CustomerGroupResult(g.id,g.cusGrpCode," +
+            "g.title," +
+            "g.createdAt)," +
+            " count(c.groupId) " +
+            "from CustomerGroup as g " +
+            "inner join Customer as c " +
+            "on g.id = c.groupId " +
+            "group by c.groupId ")
+    List<CustomerGroupResult> sortByGroup();
 }
