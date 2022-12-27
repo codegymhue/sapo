614b190429568f96f98426a00aad8c2fd85597c8
6Thienthien6 <thien502760@gmail.com>
2022-12-27 10:27:18 +0700

    cắt layout

--- sapo-entity/src/main/java/vn/sapo/entities/product/Category.java
+++ sapo-entity/src/main/java/vn/sapo/entities/product/Category.java
@@ -1,11 +1,15 @@
 package vn.sapo.entities.product;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @Accessors(chain = true)
 @Table(name = "category")
