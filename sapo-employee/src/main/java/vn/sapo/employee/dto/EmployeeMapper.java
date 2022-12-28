3d6613ff2f754f90aa59886cde16090adb7fec0b
MinhSang <dangsangb3@gmail.com>
2022-12-28 09:58:40 +0700

    hiển thị employee trong oder

--- sapo-employee/src/main/java/vn/sapo/employee/dto/EmployeeMapper.java
+++ sapo-employee/src/main/java/vn/sapo/employee/dto/EmployeeMapper.java
@@ -1,12 +1,18 @@
 package vn.sapo.employee.dto;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import vn.sapo.entities.Employee;
 
 @Component
 public class EmployeeMapper {
+    @Autowired
+    private ModelMapper modelMapper;
+
     public EmployeeResult toDTO(Employee employee) {
-        return null;
+        return modelMapper.map(employee, EmployeeResult.class)
+                .setFullName(employee.getFullName());
     }
 
     public Employee toModel(CreateEmployeeParam employeeParam) {
