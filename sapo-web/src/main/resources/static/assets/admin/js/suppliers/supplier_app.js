0dc70dccc964bbe5a23098a2c799d2bb259f2f7e
Snowmanpotato <chonloi2561998@gmail.com>
2023-02-06 13:50:56 +0700

    supplier_app.js

--- sapo-web/src/main/resources/static/assets/admin/js/suppliers/supplier_app.js
+++ sapo-web/src/main/resources/static/assets/admin/js/suppliers/supplier_app.js
@@ -3,10 +3,5 @@
 class App{
     static DOMAIN_SERVER = "http://localhost:8080";
     static PRODUCT_API = this.DOMAIN_SERVER + "/api/admin/suppliers";
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 05991ab7c9468e576cfe71cff120a2fae4e2afe8
+    
 }
