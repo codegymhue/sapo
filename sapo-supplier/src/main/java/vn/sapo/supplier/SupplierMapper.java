25f729bf83f4fc390a07df3e55b3eb579f8fcd0d
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2023-02-08 17:01:37 +0700

    tai cập nhật phần create & supplierMapper

--- sapo-supplier/src/main/java/vn/sapo/supplier/SupplierMapper.java
+++ sapo-supplier/src/main/java/vn/sapo/supplier/SupplierMapper.java
@@ -3,7 +3,9 @@ package vn.sapo.supplier;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import vn.sapo.entities.customer.CustomerStatus;
 import vn.sapo.entities.supplier.Supplier;
+import vn.sapo.entities.supplier.SupplierStatus;
 import vn.sapo.supplier.dto.CreateSupplierParam;
 import vn.sapo.supplier.dto.SupplierResult;
 import vn.sapo.supplier.dto.UpdateSupplierParam;
@@ -19,7 +21,8 @@ public class SupplierMapper {
 
 
     public Supplier toModel(CreateSupplierParam createSupplierParam) {
-        return modelMapper.map(createSupplierParam, Supplier.class);
+        return modelMapper.map(createSupplierParam, Supplier.class)
+                .setStatus(SupplierStatus.AVAILABLE);
     }
 
     public void transferFields(UpdateSupplierParam updateSupplierParam, Supplier supplier) {
