db65852f384a69388d57ad80c4e2fb75d6bb20b4
hoan_dev <nguyenthanhhoan3006@gmail.com>
2023-02-17 08:08:40 +0700

    cập nhật sửa nhà cung cấp

--- sapo-supplier/src/main/java/vn/sapo/supplier/SupplierServiceImpl.java
+++ sapo-supplier/src/main/java/vn/sapo/supplier/SupplierServiceImpl.java
@@ -69,13 +69,10 @@ public class SupplierServiceImpl implements SupplierService {
     @Override
     @Transactional
     public SupplierResult update(UpdateSupplierParam param) {
-
         Supplier supplier = supplierRepository.findById(param.getId())
                 .orElseThrow(() -> new NotFoundException("Not found supplier"));
         if (param.getGroupId() != null && supplierRepository.existsById(param.getGroupId()))
-            throw new NotFoundException("");
-
-        System.out.println(supplier);
+            throw new NotFoundException("find group supplier");
         supplierMapper.transferFields(param, supplier);
         return supplierMapper.toDTO(supplier);
     }
