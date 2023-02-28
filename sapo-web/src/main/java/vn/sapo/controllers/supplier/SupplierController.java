6f326303d897ca83911949fe51aa93afe37c3b00
Snowmanpotato <chonloi2561998@gmail.com>
2023-02-28 09:41:44 +0700

    thay đổi controller supplierList

--- sapo-web/src/main/java/vn/sapo/controllers/supplier/SupplierController.java
+++ sapo-web/src/main/java/vn/sapo/controllers/supplier/SupplierController.java
@@ -39,13 +39,9 @@ public class SupplierController {
 
     @Autowired
     PaymentMethodService paymentMethodService;
-
+    
     @GetMapping
     public ModelAndView showSupplierListPage() {
-        return new ModelAndView("/admin/suppliers/supplier_list_small");
-    }
-    @GetMapping("/small")
-    public ModelAndView showSupplierListPageSmall() {
         return new ModelAndView("/admin/suppliers/supplier_list_small2");
     }
 
