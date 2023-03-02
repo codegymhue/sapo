f4033f285c52cbec3b031d897352dc9fc0d997bb
trancu900 <pet.cu20791@gmail.com>
2023-03-02 22:12:39 +0700

    CU fix

--- sapo-entity/src/main/java/vn/sapo/entities/voucher/Voucher.java
+++ sapo-entity/src/main/java/vn/sapo/entities/voucher/Voucher.java
@@ -1,92 +1,66 @@
-// package vn.sapo.entities.voucher;
+package vn.sapo.entities.voucher;
 
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
-// import lombok.experimental.Accessors;
-// import vn.sapo.entities.*;
-// import vn.sapo.entities.order.purchase.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import vn.sapo.entities.BaseEntity;
+import vn.sapo.entities.PaymentMethod;
 
-// import javax.persistence.*;
-// import java.math.BigDecimal;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// @Accessors(chain = true)
-// @Entity
-// @Table(name = "voucher")
-// public class Voucher extends BaseEntity {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     @Column(name = "id", nullable = false)
-//     private Integer id;
-//     @Column(name = "voucher_code", unique = true)
-//     private String voucherCode;
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+@Table(name = "voucher")
+public class Voucher extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "voucher_code", unique = true)
+    private String voucherCode;
 
-//     @Column(name = "account_id", nullable = false)
-//     private Integer accountId;
+    @Column(name = "object_id", nullable = false)
+    private Integer objectId;
 
-//     @Column(name = "object_id", nullable = false)
-//     private Integer objectId;
+    @Column(name = "object_type", nullable = false)
+    private Integer objectType;
 
-//     @Column(name = "object_type", nullable = false)
-//     private Integer objectType;
+    @Column(name = "reference")
+    private Integer reference;
 
-//     @Column(name = "reference", nullable = false)
-//     private Integer reference;
+    @Column(name = "amount",nullable = false)
+    private BigDecimal amount;
 
-//     @Column(name = "amount")
-//     private Integer amount;
+    @Column(name = "note")
+    private Integer note;
 
-//     @Column(name = "account_id", nullable = false)
-//     private Integer accountId;
+    @Column(name = "account_id", nullable = false)
+    private Integer accountId;
 
-//     @Column(name = "note")
-//     private Integer note;
+    @Column(name = "group_id", nullable = false)
+    private Integer groupId;
 
-//     @ManyToOne(fetch = FetchType.LAZY)
-//     @JoinColumn(name = "assignee id_id")
-//     private Employee employee;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "payment_method_id", foreignKey = @ForeignKey(name = "fk_voucher_payment_method"))
+    private PaymentMethod paymentMethod;
 
-//     @Column(name = "employee_id", insertable = false, updatable = false)
-//     private Integer employeeId;
+    @Column(name = "payment_method_id", insertable = false, updatable = false)
+    private String paymentMethodId;
 
-//     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//     @JoinColumn(name = "payment_method_id", foreignKey = @ForeignKey(name = "fk_voucher_payment_method"))
-//     private PaymentMethod paymentMethod;
 
-//     @Column(name = "payment_method_id", insertable = false, updatable = false)
-//     private String paymentMethodId;
-//     @ManyToOne(fetch = FetchType.LAZY)
-//     @JoinColumn(name = "purchase_order_id")
-//     private PurchaseOrder purchaseOrder;
+    public Voucher(Integer id) {
+        this.id = id;
+    }
 
-//     @Column(name = "purchase_order_id", insertable = false, updatable = false)
-//     private Integer purchaseOrderId;
+    public Voucher setPaymentMethodId(String paymentMethodId) {
+        this.paymentMethod = new PaymentMethod(this.paymentMethodId = paymentMethodId);
+        return this;
+    }
 
-//     public Voucher(Integer id) {
-//         this.id = id;
-//     }
 
-//     public Voucher setEmployeeId(Integer employeeId) {
-//         this.employee = new Employee(this.employeeId = employeeId);
-//         return this;
-//     }
-
-//     public Voucher setPaymentMethodId(String paymentMethodId) {
-//         this.paymentMethod = new PaymentMethod(this.paymentMethodId = paymentMethodId);
-//         return this;
-//     }
-
-//     public Voucher setPurchaseOrderId(Integer paymentMethodId) {
-//         this.purchaseOrder = new PurchaseOrder(this.purchaseOrderId = paymentMethodId);
-//         return this;
-//     }
-
-//     public Voucher(Integer employeeId, String paymentMethodId, Integer purchaseOrderId) {
-//         setEmployeeId(employeeId);
-//         setPaymentMethodId(paymentMethodId);
-//         setPurchaseOrderId(purchaseOrderId);
-//     }
-// }
+}
