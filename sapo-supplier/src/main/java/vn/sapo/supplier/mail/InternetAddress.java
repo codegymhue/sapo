530a89af4be734231cb524bd5519cba4f679f567
master_dev <tanluong@gmail.com>
2023-03-09 17:40:29 +0700

    commit excel

---
+++ sapo-supplier/src/main/java/vn/sapo/supplier/mail/InternetAddress.java
@@ -0,0 +1,9 @@
+//package vn.sapo.supplier.mail;
+//
+//public class InternetAddress {
+//    public InternetAddress(String s) {
+//    }
+//
+//    public static void parse(String s) {
+//    }
+//}
