9abc94eae28fcfa0dc62e0f7df6bbe08de3daf1b
Snowmanpotato <chonloi2561998@gmail.com>
2023-03-09 17:37:37 +0700

    thêm phần loading, chỉnh sửa css table, chỉnh sửa checkbox, xong phần điều chỉnh cột, thêm bộ lọc tags

--- sapo-supplier/src/main/java/vn/sapo/supplier/SupplierFilterRepository.java
+++ sapo-supplier/src/main/java/vn/sapo/supplier/SupplierFilterRepository.java
@@ -1,17 +1,15 @@
 package vn.sapo.supplier;
 
-import org.hibernate.Session;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.provider.HibernateUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import vn.sapo.entities.supplier.Supplier;
-import vn.sapo.entities.supplier.SupplierGroup;
 import vn.sapo.supplier.dto.SupplierFilter;
-import vn.sapo.supplierGroup.dto.SupplierGroupResult;
 
-import javax.persistence.criteria.*;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
@@ -40,68 +38,34 @@ public interface SupplierFilterRepository extends JpaRepository<Supplier, Intege
                 predicates.add(predicate);
             }
 
-            //hàm này xài được
-//            Predicate inJsonNumbers = criteriaBuilder
-//                    .function("JSON_EXTRACT",
-//                            String.class,
-//                            root.get("tags"),
-//                            criteriaBuilder.literal("$[*]")
-//                          )
-//                    .in(filter.getTags());
-//            predicates.add(inJsonNumbers);
-//
-//            Predicate containsTag = (Predicate) criteriaBuilder
-//                    .function(
-//                            "JSON_CONTAINS",
-//                            Boolean.class,
-//                            root.get("tags"),
-//                            criteriaBuilder.literal("\"" + filter.getTags() + "\"")
-//                    );
-//            predicates.add(containsTag);
+            List<String> tagsToSearch = filter.getTags();
+            Predicate tagsPredicate = null;
 
+            if (!tagsToSearch.isEmpty()) {
+                Expression<Boolean> fistTag = criteriaBuilder
+                        .function("JSON_CONTAINS",
+                                Boolean.class,
+                                root.get("tags"),
+                                criteriaBuilder.literal('"' + tagsToSearch.get(0) + '"')
+                        );
+                tagsPredicate = criteriaBuilder.isTrue(fistTag);
 
-//            Predicate likeJsonNumbers = criteriaBuilder
-//                    .like(
-//                            criteriaBuilder.function(
-//                                    "JSON_EXTRACT",
-//                                    String.class,
-//                                    root.get("tags"),
-//                                    criteriaBuilder.literal("$[*]")
-//                            ),
-//                            "%" + filter.getTags() + "%"
-//                    );
-//            predicates.add(likeJsonNumbers);
+                for (int i = 1; i < tagsToSearch.size(); i++) {
+                    String remainingTags = tagsToSearch.get(i);
 
+                    Expression<Boolean> leftoverTags = criteriaBuilder
+                            .function("JSON_CONTAINS",
+                                    Boolean.class,
+                                    root.get("tags"),
+                                    criteriaBuilder.literal('"' + remainingTags + '"')
+                            );
+                    tagsPredicate = criteriaBuilder.or(tagsPredicate, criteriaBuilder.isTrue(leftoverTags));
+                }
+            }
 
-
-//            // Tìm kiếm theo tags
-//            if (!filter.getTags().isEmpty()) {
-//                Expression<String> jsonExpr = root.get("tags");
-//                for (String tag : filter.getTags()) {
-//                    Predicate predicate = criteriaBuilder.like(jsonExpr.as(String.class), "%\"" + tag + "\"%");
-//                    predicates.add(predicate);
-//                }
-//            }
-//            Predicate tagsPredicate = null;
-//            if (!filter.getTags().isEmpty()) {
-//                tagsPredicate = criteriaBuilder.or(root.get("tags").in(filter.getTags())) ;
-//            }
-//
-//            if (tagsPredicate != null) {
-//                predicates.add(tagsPredicate);
-//            }
-
-//            Predicate tagsPredicate = criteriaBuilder.like(
-//                    criteriaBuilder.function("JSON_EXTRACT", String.class, root.get("tags"), criteriaBuilder.literal("$.tags")),
-//                    "%" + filter.getTags() + "%");
-//
-//            for (int i = 1; i < tagsToSearch.size(); i++) {
-//                String tag = tagsToSearch.get(i);
-//                tagsPredicate = builder.or(tagsPredicate, builder.like(
-//                        builder.function("JSON_EXTRACT", String.class, root.get("tags"), builder.literal("$.tags")),
-//                        "%" + tag + "%"));
-//            }
-
+            if (tagsPredicate != null) {
+                predicates.add(tagsPredicate);
+            }
 
             if (!filter.getStatuses().isEmpty()) {
                 Predicate predicate = criteriaBuilder.or(root.get("status").in(filter.getStatuses()));
