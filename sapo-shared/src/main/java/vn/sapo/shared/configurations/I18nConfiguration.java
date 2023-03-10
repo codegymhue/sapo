037acf5666246247f32038ac58f240a313e1c339
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2023-03-10 08:54:24 +0700

    cu implete i18n

---
+++ sapo-shared/src/main/java/vn/sapo/shared/configurations/I18nConfiguration.java
@@ -0,0 +1,33 @@
+package vn.sapo.shared.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class I18nConfiguration {
+
+    public static final String MODEL_MAPPER_SKIP_NULL_ENABLED = "MODEL_MAPPER_SKIP_NULL_ENABLED";
+    public static final String MODEL_MAPPER_SKIP_NULL_DISABLED = "MODEL_MAPPER_SKIP_NULL_DISABLED";
+
+    @Bean(MODEL_MAPPER_SKIP_NULL_ENABLED)
+    @Primary
+    public ModelMapper getModelMapperSkipNullEnabled() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper;
+    }
+
+    @Bean(MODEL_MAPPER_SKIP_NULL_DISABLED)
+    public ModelMapper getModelMapperSkipNullDisabled() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper;
+    }
+
+}
