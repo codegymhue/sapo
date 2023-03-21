950316050dea38ba5b93fcb5566de0a8c7c7c86a
TrungTran2751999 <trantrung2751999@gmail.com>
2023-03-21 08:16:23 +0700

    update customerAPI, modal-update-customer, customerUpdateSerries

---
+++ sapo-customer/src/main/java/vn/sapo/customer/dto/CustomerUpdateSeries.java
@@ -0,0 +1,13 @@
+package vn.sapo.customer.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CustomerUpdateSeries {
+//    private Integer employeeId;
+//    private String
+}
